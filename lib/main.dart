d7035f6bce589c7db517194605a17df0da1a29d8
Partho Debnath <parthodebnath28@gmail.com>
2023-08-23 02:38:17 +0600

    get Match list from Firebase and display Match option added.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -10,7 +17,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Football Live Score',
       home: MyHomePage(),
     );
   }
@@ -24,16 +32,131 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Football Matches'),
+        title: const Text('Matche List'),
       ),
-      body: const Center(
-        child: Text(
-          'Football Matches.',
+      body: StreamBuilder(
+        stream: firestore.collection('footballmatches').snapshots(),
+        builder: (cntxt, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError == true) {
+            return Center(child: Text(snapshot.error.toString()));
+          } else if (snapshot.hasData && snapshot.data?.docs.isEmpty == true) {
+            return const Center(child: Text('No Match.'));
+          } else if (snapshot.hasData && snapshot.data!.docs.isNotEmpty) {
+            // log(snapshot.data!.docs.length.toString());
+            return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (cntxt, index) {
+                return ListTile(
+                  title: Text(
+                      '${snapshot.data!.docs[index].get('team_a')} vs ${snapshot.data!.docs[index].get('team_b')}'),
+                  trailing: const Icon(Icons.arrow_right_alt_outlined),
+                  onTap: () {
+                    navigateToMatchDetailsScreen(snapshot.data!.docs[index].id);
+                  },
+                );
+              },
+            );
+          }
+          return const SizedBox();
+        },
+      ),
+    );
+  }
+
+  void navigateToMatchDetailsScreen(String matchId) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (cntxt) => MatchDetails(
+          matchId: matchId,
         ),
+      ),
+    );
+  }
+}
+
+class MatchDetails extends StatelessWidget {
+  final String matchId;
+  MatchDetails({super.key, required this.matchId});
+
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(matchId),
+      ),
+      body: StreamBuilder(
+        stream:
+            firestore.collection('footballmatches').doc(matchId).snapshots(),
+        builder: (cntxt, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError == true) {
+            return Center(child: Text(snapshot.error.toString()));
+          } else if (snapshot.hasData && snapshot.data!.exists == false) {
+            return const Center(child: Text('No Match.'));
+          } else if (snapshot.hasData) {
+            // log(snapshot.data!.data().toString());
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                elevation: 5,
+                child: SizedBox(
+                  height: 120,
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          "${snapshot.data!.get('team_a')} vs ${snapshot.data!.get('team_b')}",
+                          style: Theme.of(context)
+                              .textTheme
+                              .headlineMedium!
+                              .copyWith(
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                              ),
+                        ),
+                        Text(
+                          "${snapshot.data!.get('score_team_a')} : ${snapshot.data!.get('score_team_b')}",
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          "Time : ${snapshot.data!.get('time')}",
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          "Total Time : ${snapshot.data!.get('total_time')}",
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            );
+          }
+          return const SizedBox();
+        },
       ),
     );
   }
